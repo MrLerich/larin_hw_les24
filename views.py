d39358863db61f8d93b5f87385ec58bf40717afe
MrLerich <93153582+MrLerich@users.noreply.github.com>
2022-10-03 17:09:34 +0300

    init 2

--- views.py
+++ views.py
@@ -1,9 +1,21 @@
 from flask import Blueprint
+from marshmallow import ValidationError
 
 app_bp = Blueprint('main', __name__)
 
 @app_bp.route("/perform_query", methods=['POST'])
 def perform_query():
+    try:
+        params = BatchRequestParams().load(request.json)
+
+    except ValidationError as e:
+        return e.message, 404
+
+    result = None
+    for query in params['queries']:
+        result = build_query(cmd=query['cmd'], param=query['param'], data=result)
+
+    return jsonify(result)
 
 
     # получить параметры query и file_name из request.args, при ошибке вернуть ошибку 400
