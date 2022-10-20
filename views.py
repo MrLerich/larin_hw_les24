c23243506a78cd8ba499892fc60af4f2eaf03a3a
MrLerich <mrlerich@gmail.com>
2022-10-20 13:37:48 +0300

    add unit+requirements

--- views.py
+++ views.py
@@ -1,5 +1,3 @@
-from typing import List, Optional, Any, Mapping, Union, Iterable
-
 from flask import Blueprint, jsonify, request, Response
 from marshmallow import ValidationError
 
@@ -9,7 +7,7 @@ from models import BatchRequestParams
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route("/perform_query", methods=['POST'])
-def perform_query(): #type: ignore
+def perform_query():
     try:
         params = BatchRequestParams().load(request.json)
 
